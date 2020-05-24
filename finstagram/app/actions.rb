e95fd43759a63f5fcb6fadd4bbffe5bc46dce330
Yunhao Xu <61392789+kadenXu@users.noreply.github.com>
2020-05-23 18:20:18 -0700

    Add files via upload

---
+++ finstagram/app/actions.rb
@@ -0,0 +1,162 @@
+# get "/" do
+#    File.read(File.join("app/views", "index.html"))
+# end
+# def humanized_ago_in_minutes(time_ago_in_minutes)
+#     if time_ago_in_minutes >= 60
+#         "#{time_ago_in_minutes / 60} hours ago"
+#     else
+#         "#{time_ago_in_minutes} minutes ago"
+#     end
+# end
+
+
+# get '/' do
+    # @finstagram_post_shark = {
+    #     username: "sharky_j",
+    #     avatar_url: "http://naserca.com/images/sharky_j.jpg",
+    #     photo_url: "http://naserca.com/images/shark.jpg",
+    #     humanized_ago_in_minutes: humanized_ago_in_minutes(15),
+    #     like_count: 0,
+    #     comment_count: 1,
+    #     comments: [{
+    #         username: "sharky_j",
+    #         text: "Out for the long weekend... too embarrassed to show y'all the beach bod!"
+    #     }]
+    # }
+    #
+    # @finstagram_post_whale = {
+    #     username: "kirk_whalum",
+    #     avatar_url:"http://naserca.com/images/kirk_whalum.jpg",
+    #     photo_url: "http://naserca.com/images/whale.jpg",
+    #     humanized_ago_in_minutes: humanized_ago_in_minutes(65),
+    #     like_count: 0,
+    #     comment_count: 1,
+    #     comments: [{
+    #         username:"kirk_whalum",
+    #         text: "#weekendvibes"
+    #     }]
+    # }
+    #
+    # @finstagram_post_marlin = {
+    #     username: "marlin_peppa",
+    #     avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
+    #     photo_url: "http://naserca.com/images/marlin.jpg",
+    #     humanized_ago_in_minutes: humanized_ago_in_minutes(190),
+    #     like_count:0,
+    #     comment_count: 1,
+    #     comments: [{
+    #         username: "marlin_peppa",
+    #         text: "lunchtime! ;)"
+    #     }]
+    # }
+    #
+    # @finstagram_posts = [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
+helpers do
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+end
+
+get '/' do
+    @finstagram_posts = FinstagramPost.order(created_at: :desc)
+    erb(:index)
+end
+
+get '/signup' do
+  @user = User.new
+  erb(:signup)
+end
+
+post '/signup' do
+
+  # grab user input values from params
+  email      = params[:email]
+  avatar_url = params[:avatar_url]
+  username   = params[:username]
+  password   = params[:password]
+
+
+  # instantiate and save a User
+  @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+
+  if @user.save
+  redirect to('/login')
+  else
+    erb(:signup)
+  end
+end
+
+get '/login' do
+  erb(:login)
+end
+
+post '/login' do
+
+  username = params[:username]
+  password = params[:password]
+
+  @user = User.find_by(username: username)
+
+
+  if @user && @user.password == password
+    session[:user_id] = @user.id
+    redirect to('/')
+  else
+      @error_message = "Login failed."
+      erb(:login)
+  end
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect to('/')
+end
+
+get '/finstagram_posts/new' do
+  @finstagram_post = FinstagramPost.new
+  erb(:"finstagram_posts/new")
+end
+
+post '/finstagram_posts' do
+  photo_url = params[:photo_url]
+
+  @finstagram_post = FinstagramPost.new({ photo_url:photo_url, user_id:current_user.id })
+
+  if @finstagram_post.save
+    redirect(to('/'))
+  else
+    @finstagram_post.errors.full_messages.inspect
+    erb(:"finstagram_posts/new")
+  end
+end
+
+get '/finstagram_posts/:id' do
+  @finstagram_post = FinstagramPost.find(params[:id])
+  erb(:"finstagram_posts/show")
+end
+
+post '/comments' do
+  text = params[ :text]
+  finstagram_post_id = params[ :finstagram_post_id]
+
+  comment = Comment.new({ text: text, finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+
+  comment.save
+
+  redirect(back)
+end
+
+post '/likes' do
+  finstagram_post_id = params[:finstagram_post_id]
+
+  like = Like.new({ finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+  like.save
+
+  redirect(back)
+end
+
+delete '/likes/:id' do
+  like = Like.find_by(params[:id])
+  like.destroy
+  redirect(back)
+end
